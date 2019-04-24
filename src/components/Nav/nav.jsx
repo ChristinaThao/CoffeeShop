1a6b2781d209de40491908d1f1cc0d9b0a32fe63
Christina <christinathao_714@hotmail.com>
2019-04-24 16:50:27 -0400

    Style change

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import {Link}  from 'react-router-dom';
-import './nav.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./nav.css";
 
 export default class Nav extends React.Component {
-    render() {
-        return (
-            <div style={{display:"flex"}}>
-                <div className="nav left">
-                    <Link to="/">Coffee</Link>
-                </div>
-                <div className="nav right">
-                    <Link to="/menu" className="option">Menu</Link>
-                    <Link to="/reservation" className="option">Reservation</Link>
-                    <Link to="/shop" className="option">Shop</Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <div className="nav left">
+          <Link to="/">Coffee</Link>
+        </div>
+        <div className="nav right">
+          <Link to="/shop" className="option">
+            Shop
+          </Link>
+          <Link to="/reservation" className="option">
+            Reservation
+          </Link>
+          <Link to="/menu" className="option">
+            Menu
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,8 @@ export default class Home extends React.Component {
     render() {
         return(
             <div>
-                <div className="top" style={{backgroundImage: 'url(' + require('../../img/bakery.jpg') + ')'}}>
-                    <h1>Home</h1>
+                <div className="top" style={{backgroundImage: 'url(' + require('../../img/coffee-pour.jpg') + ')'}}>
+                    
                 </div>
                 <div className="body">
                     <p>
