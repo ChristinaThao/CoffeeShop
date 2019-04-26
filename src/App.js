a7d2e9c570de7cb4581f325d0b65da8b1b5996eb
Christina <christinathao_714@hotmail.com>
2019-04-26 14:34:42 -0400

    Change in content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "../src/pages/Home/Home";
-import Menu from "../src/pages/Menu/Menu";
-import Nav from "../src/components/Nav/nav";
+import Nav from "../src/components/nav/nav";
+import Drinks from "./pages/Drinks/Drinks";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       </div>
       <Switch className="pages">
         <Route exact path="/" component={Home} />
-        <Route path="/menu" component={Menu} />
+        <Route path="/drinks" component={Drinks} />
       </Switch>
     </Router>
   );
